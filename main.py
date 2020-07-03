f79b3c95c820d7fb3cf975994b285c8c04614e51
alexandru-robertcroitoru <andy_robert97@yahoo.com>
2020-07-03 10:25:59 +0200

    Update2

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 import os
-os.system("conda activate tensorflow")
-os.system("jupyter notebook")
+#os.system("conda activate tensorflow")
+#os.system("jupyter notebook")
+os.system("echo hello world")
